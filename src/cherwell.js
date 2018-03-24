d713f39350489261415e144393bc535b71a6e388
Emilia Lewandowska <emrossa@gmail.com>
2018-03-24 14:59:47 +0000

    Split out stub devices and users

--- src/cherwell.js
+++ src/cherwell.js
@@ -1,14 +1,5 @@
-var devices = [{
-    hostname: 'SBGML011111',
-    macAddress: '4C:32:75:8B:88:25',
-    owner: 123456
-}];
-
-var users = [{
-    name: 'Emilia Lewandowska',
-    emailAddress: 'emrossa@gmail.com',
-    id: 123456
-}];
+var devices = require('../data/stub/devices.json');
+var users = require('../data/stub/users.json');
 
 module.exports = {
     getDevicesByOwner: function (owner) {
