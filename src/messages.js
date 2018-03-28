5828db837b44f86c390dcec21a47de345c5f9f29
Emilia Lewandowska <emrossa@gmail.com>
2018-03-28 15:32:09 +0100

    Return early for invalid user name

--- src/messages.js
+++ src/messages.js
@@ -6,47 +6,46 @@ var sessions = require('./sessions');
 var bot;
 
 function locateUser(user, callback) {
-    if (/^<@[A-Z0-9]+>$/.test(user)) {
-        var userId = user.replace(/[<@>]/g, '');
-        users.isOptedIn(userId, function (err, isOptedIn) {
-            if (err) {
-                return callback(err);
+    if (!/^<@[A-Z0-9]+>$/.test(user)) {
+        return callback(new Error('invalid_user'), '“' + user + '” is not a user');
+    }
+
+    var userId = user.replace(/[<@>]/g, '');
+    users.isOptedIn(userId, function (err, isOptedIn) {
+        if (err) {
+            return callback(err);
+        }
+
+        if (!isOptedIn) {
+            return callback(err, 'Sorry, ' + user + ' is not opted in');
+        }
+
+        bot.getUserById(userId).then(function (slackUser) {
+            var cherwellUser = cherwell.getUserByEmail(slackUser.profile.email);
+            if (!cherwellUser) {
+                return callback(new Error('no_user'), 'I can’t find ' + user + ' in Cherwell!');
             }
 
-            if (!isOptedIn) {
-                return callback(err, 'Sorry, ' + user + ' is not opted in');
+            var devices = cherwell.getDevicesByOwner(cherwellUser);
+            if (devices.length == 0) {
+                return callback(new Error('no_devices'), 'I can’t find any devices for ' + user);
             }
 
-            bot.getUserById(userId).then(function (slackUser) {
-                var cherwellUser = cherwell.getUserByEmail(slackUser.profile.email);
-                if (!cherwellUser) {
-                    return callback(new Error('no_user'), 'I can’t find ' + user + ' in Cherwell!');
-                }
+            var session = sessions.getLatest(devices[0].macAddress);
+            if (!session) {
+                return callback(new Error('no_events'), 'I haven’t seen ' + user);
+            }
 
-                var devices = cherwell.getDevicesByOwner(cherwellUser);
-                if (devices.length == 0) {
-                    return callback(new Error('no_devices'), 'I can’t find any devices for ' + user);
-                }
-
-                var session = sessions.getLatest(devices[0].macAddress);
-                if (!session) {
-                    return callback(new Error('no_events'), 'I haven’t seen ' + user);
-                }
-
-                callback(null, util.format(
-                    'I last saw %s %s at %s',
-                    user,
-                    session.getRelativeTime(),
-                    routers.get(session.getRouter()).getName()
-                ));
-            }).catch(function (error) {
-                console.error('Error', error);
-            });
+            callback(null, util.format(
+                'I last saw %s %s at %s',
+                user,
+                session.getRelativeTime(),
+                routers.get(session.getRouter()).getName()
+            ));
+        }).catch(function (error) {
+            console.error('Error', error);
         });
-    }
-    else {
-        callback(new Error('invalid_user'), '“' + user + '” is not a user');
-    }
+    });
 }
 
 function optIn(user, callback) {
